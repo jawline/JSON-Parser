b5ccd2cd4d1a8542f8f5d16cda64440b48917457
Blake Loring <blake@parsed.uk>
2020-12-20 15:31:08 +0800

    Cleanup

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 import Data.String (String)
 import Data.Map (Map, empty, insert)
+import Data.List (isPrefixOf)
 
 {-- Helper Methods --}
 
@@ -37,18 +38,29 @@ data JSValue = JSObject (Map String JSValue)
   | JSUndefined
   deriving Show
 
+-- If 'null' return JSNull and drop the keyword from the string.
+parseNull :: String -> Maybe (JSValue, String)
+parseNull xs
+  | isPrefixOf "null" xs = Just (JSNull, drop 4 xs)
+  | otherwise = Nothing
+
+-- If 'true' or 'false' return a JSBool and drop the keyword from the string. Otherwise return Nothing.
+parseBool :: String -> Maybe (JSValue, String)
+parseBool xs
+  | isPrefixOf "true" xs = Just (JSBool True, drop 4 xs)
+  | isPrefixOf "false" xs = Just (JSBool False, drop 5 xs)
+  | otherwise = Nothing
+
 extractInteger :: String -> (String, String)
 extractInteger [] = ([], [])
 extractInteger (x:xs)
-  | digit x = (x:follows, rest)
+  | digit x, (follows, xs) <- extractInteger xs = (x:follows, xs)
   | otherwise = ([], x:xs)
-  where
-    (follows, rest) = extractInteger xs
 
+-- Extract a double (number in the format 523123 or 52131.4231 with an optional - in front)
 parseDouble :: String -> (Double, String)
-parseDouble ('-':xs) = (-result, rest)
-  where (result, rest) = parseDouble xs
 parseDouble xs
+  | ('-':xs) <- xs, (h, r) <- parseDouble xs = (-h, r) -- If our number starts with a - then negate it
   | afterFraction /= [] && charAfterFraction == '.' = (read (fraction ++ "." ++ exponent) :: Double, afterExponent)
   | otherwise = (read fraction :: Double, afterFraction)
   where
@@ -56,61 +68,68 @@ parseDouble xs
     (charAfterFraction:remaining) = afterFraction
     (exponent, afterExponent) = extractInteger remaining
 
-parseNumber :: String -> (JSValue, String)
-parseNumber xs = (JSNumber result, rest)
-  where (result, rest) = parseDouble xs
+-- If what follows is a digit ot a - then start parsing a number, otherwise Nothing
+parseNumber :: String -> Maybe (JSValue, String)
+parseNumber xs
+  | (head xs) == '-' || digit (head xs) = Just (JSNumber result, rest)
+  | otherwise = Nothing
+  where
+    (result, rest) = parseDouble xs
 
+-- Parse the string after the opening " until we find a closing " that isn't escaped
 parseStringInner :: String -> (String, String)
-parseStringInner ('"':xs) = ("", xs)
-parseStringInner ('\\':r:xs) = (escaped r:str, follows)
-    where (str, follows) = parseStringInner xs
-parseStringInner (x:xs) = (x:str, follows)
-    where (str, follows) = parseStringInner xs
+parseStringInner xs
+  | ('"':xs) <- xs = ("", xs)
+  | ('\\':r:xs) <- xs, (str, follows) <- parseStringInner xs = (escaped r:str, follows)
+  | (x:xs) <- xs, (str, follows) <- parseStringInner xs = (x:str, follows)
 
-parseString :: String -> (JSValue, String)
-parseString xs = (JSString str, rest)
-  where (str, rest) = parseStringInner xs
+-- If the next thing starts with a " then we parse it as a string, otherwise Nothing
+parseString :: String -> Maybe (JSValue, String)
+parseString xs
+  | ('"':xs) <- xs, (str, rest) <- parseStringInner xs = Just (JSString str, rest)
+  | otherwise = Nothing
 
 parseArrayInner :: String -> ([JSValue], String)
-parseArrayInner (']':xs) = ([], xs)
 parseArrayInner xs
-  | followingChar == ',' = (parsedItem:recursed, recursedRest)
-  | followingChar == ']' = ([parsedItem], rest)
+  | (',':rest) <- rest, (recursed, recursedRest) <- parseArrayInner (skip rest) = (parsedItem:recursed, recursedRest)
+  | (']':rest) <- rest = ([parsedItem], rest)
   where
     (parsedItem, afterParsed) = parseJson xs
-    (followingChar:rest) = skip afterParsed
-    (recursed, recursedRest) = parseArrayInner (skip rest)
+    rest = skip afterParsed
 
-parseArray :: String -> (JSValue, String)
-parseArray xs = (JSArray arr, rest)
-  where (arr, rest) = parseArrayInner (skip xs)
+-- If the next thing starts with a '[' then we parse it as a array, otherwise Nothing
+parseArray :: String -> Maybe (JSValue, String)
+parseArray xs
+  | ('[':xs) <- xs, (']':xs) <- skip xs = Just (JSArray [], xs)
+  | ('[':xs) <- xs, (arr, rest) <- parseArrayInner (skip xs) = Just (JSArray arr, rest)
+  | otherwise = Nothing 
 
 parseObjectInner :: String -> ((Map String JSValue), String)
-parseObjectInner ('}':xs) = (empty, xs)
 parseObjectInner xs
-  | followingChar == ',' = (insert parsedName parsedValue recursed, recursedRest)
-  | followingChar == '}' = (insert parsedName parsedValue empty, rest)
+  | (',':rest) <- rest, (recursed, recursedRest) <- parseObjectInner rest = (insert parsedName parsedValue recursed, recursedRest)
+  | ('}':rest) <- rest = (insert parsedName parsedValue empty, rest)
   where
     ('"':nameStart) = xs
     (parsedName, afterName) = parseStringInner nameStart
     (':':followingName) = skip afterName
     (parsedValue, afterValue) = parseJson followingName
-    (followingChar:rest) = skip afterValue
-    (recursed, recursedRest) = parseObjectInner (skip rest)
+    rest = skip afterValue
 
-parseObject :: String -> (JSValue, String)
-parseObject xs = (JSObject map, rest)
-  where (map, rest) = parseObjectInner (skip xs)
+-- If the next thing starts with a '{' then we parse it as an object, otherwise Nothing
+parseObject :: String -> Maybe (JSValue, String)
+parseObject xs
+  | ('{':xs) <- xs, ('}':xs) <- skip xs = Just (JSObject empty, xs)
+  | ('{':xs) <- xs, (map, rest) <- parseObjectInner (skip xs) = Just (JSObject map, rest)
+  | otherwise = Nothing
 
 parseJsonInner :: String -> (JSValue, String)
-parseJsonInner ('n':'u':'l':'l':xs) = (JSNull, xs)
-parseJsonInner ('t':'r':'u':'e':xs) = (JSBool True, xs)
-parseJsonInner ('f':'a':'l':'s':'e':xs) = (JSBool False, xs)
-parseJsonInner (x:xs)
-  | x == '-' || digit x = parseNumber(x:xs)
-  | x == '"' = parseString xs
-  | x == '[' = parseArray xs
-  | x == '{' = parseObject xs
+parseJsonInner xs
+  | Just jsNull <- parseNull xs = jsNull
+  | Just jsBool <- parseBool xs = jsBool
+  | Just jsNumber <- parseNumber xs = jsNumber
+  | Just jsStr <- parseString xs = jsStr
+  | Just jsArr <- parseArray xs = jsArr
+  | Just jsObj <- parseObject xs = jsObj
 
 parseJson :: String -> (JSValue, String)
 parseJson xs = parseJsonInner (skip xs)
@@ -122,9 +141,10 @@ main = do
   _ <- putStrLn (show (parseJson "null"))
   _ <- putStrLn (show (parseJson "false"))
   _ <- putStrLn (show (parseJson "true"))
-  _ <- putStrLn (show (parseJson "-5432.69        "))
   _ <- putStrLn (show (parseJson "523"))
+  _ <- putStrLn (show (parseJson "-5432.69        "))
   _ <- putStrLn (show (parseJson "-96"))
+  _ <- putStrLn (show (parseJson "\"Hello\""))
   _ <- putStrLn (show (parseJson "        \"Hello world \r\n\bQQQ 523\""))
   _ <- putStrLn (show (parseJson "[]"))
   _ <- putStrLn (show (parseJson "[true]"))
